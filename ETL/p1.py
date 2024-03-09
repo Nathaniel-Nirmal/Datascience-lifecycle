6bb9606a952adcdf8eda741e717f6e59ab9bb0f6
Akhil Raj <ar2427@cornell.edu>
2024-03-08 22:27:37 -0500

    transform from nhis to brfss implemented

--- ETL/p1.py
+++ ETL/p1.py
@@ -1,6 +1,10 @@
 import sys
 from pyspark.sql import SparkSession
 import argparse
+from pyspark.sql.types import *
+# Convert function to udf
+from pyspark.sql.functions import col, udf, array
+
 #feel free to def new functions if you need
 
 def create_dataframe(filepath, format, spark):
@@ -24,6 +28,32 @@ def create_dataframe(filepath, format, spark):
 
     return spark_df
 
+def tranformAgeFromNhisToBrfss(age, ageDict = {1 : (18, 24), 2 : (25, 29), 3 : (30, 34), 4 : (35, 39), 5 : (40, 44), 6 : (45, 49), 7 : (50, 54), 8 : (55, 59), 9 : (60, 64), 10 : (65, 69), 11 : (70, 74), 12 : (74, 79), 13 : (80, 99)}):
+    if age is None:
+        return 14.0
+    for key in ageDict.keys():
+        if ageDict[int(key)][0] <= int(age) <= ageDict[int(key)][1]:
+            return float(key)
+    raise ValueError("Age is neither None nor within possible ranges. This is not what is expected. Add code to handle this. Age : ", age)
+
+def tranformRaceFromNhisToBrfss(race):
+    MRACBPI2 = int(race[0])
+    HISPAN_I = int(race[1])
+    raceMap = {1.0 : (1, 12), 2.0 : (2, 12), 3.0 : ((6, 7, 12), 12), 4.0 : (3, 12), 5.0 : (-1, tuple([i for i in range(12)])), 6.0 : (16, 12)} # -1 : any value will do. This map maps race value from brfss's race column(_IMPRACE) to values in columns of Nhis containit races(MRACBPI2 and HISPAN_I)
+    if MRACBPI2 is None or HISPAN_I is None:
+        return None
+    for key in raceMap.keys():
+        if type(raceMap[key][0]) == tuple:
+            for subkey in raceMap[key][0]:
+                if (MRACBPI2 == int(subkey)) and HISPAN_I == raceMap[key][1]:
+                    return key
+        elif type(raceMap[key][1]) == tuple:
+            for subkey in raceMap[key][1]:
+                if HISPAN_I == int(subkey):
+                    return key
+        elif MRACBPI2 == raceMap[key][0] and HISPAN_I == raceMap[key][1]:
+            return key
+    raise ValueError(f"Both MRACBPI2 and HISPAN_I are non-null but no brfss match is their as per raceMap. This is not what is expected. Add code to handle this. MRACBPI2 : {MRACBPI2}, and HISPAN_I : {HISPAN_I} and race(combined) : {race}")
 
 def transform_nhis_data(nhis_df):
     """
@@ -34,11 +64,14 @@ def transform_nhis_data(nhis_df):
     """
 
     #add your code here
-    transformed_df =  None#temporary placeholder
-    raceMap = {1.0 : (1, 12), 2.0 : (2, 12), 3.0 : ((6, 7, 12), 12), 4.0 : (3, 12), 5.0 : (-1, tuple([i for i in range(12)])), 6.0 : (16, 12)} # -1 : any value will do.
-    
-    print(nhis_df.printSchema())
-    
+
+    # nhis_df = nhis_df.withColumn("SEX", nhis_df.SEX.cast(DoubleType()))
+    tranformAgeFromNhisToBrfssUDF = udf(lambda x:tranformAgeFromNhisToBrfss(x),StringType()) # UDF function for age column coversion in NHIS data 
+    tranformRaceFromNhisToBrfssUDF = udf(lambda x:tranformRaceFromNhisToBrfss(x),StringType()) # UDF function for conversion of columns in NHIS contain race info to column in Brfss containing race info
+   
+
+    transformed_df = nhis_df.select(nhis_df.SEX.cast(DoubleType()), tranformAgeFromNhisToBrfssUDF(col("AGE_P")).cast(DoubleType()).alias("_AGEG5YR"), tranformRaceFromNhisToBrfssUDF(array(col("MRACBPI2"), col("HISPAN_I"))).alias('_IMPRACE').cast(DoubleType()), nhis_df.DIBEV1.cast(DoubleType())) # Transforming structure of nhis to brfss
+
     return transformed_df
 
 
@@ -81,9 +114,9 @@ if __name__ == '__main__':
         arg_parser.usage = arg_parser.format_help()
         arg_parser.print_usage()
     else:
-        brfss_filename = args.nhis
-        nhis_filename = args.brfss
-
+        brfss_filename = args.brfss
+        nhis_filename = args.nhis
+        
         # Start spark session
         spark = SparkSession.builder.getOrCreate()
 
